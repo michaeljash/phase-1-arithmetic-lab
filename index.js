48ecde3bc9f18f05f0b95f35e39739841788046b
jashonmichael <jashon.michael@student.moringaschool.com>
2024-03-20 14:13:22 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
+const num1 = 31;
+const num2 = 2;
 
-// Write your code here
+const multiply = num1 * num2
+
+console.log ('multiply:${multiply}');
+
+
+const random = math.floor(math.random() * Number.MAX_SAFE_INTEGER) + 1;
+
+console.log('random: ${random}');
+
+const num3 = 22;
+const num4 = 6;
+const mod = num3 % num4;
+
+console.log('mod: ${mod}');
+
+const numbers = [10, 5, 20];
+const max = Math.max(...numbers);
+
+console.log('max: ${max}');
